7ad0c510a31d37033993d22ad78717a36fece30e
fostervip <126537293+fostervip@users.noreply.github.com>
2025-04-12 14:53:35 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -745,7 +745,7 @@ main() {
     set_build_signature
     fix_compile_vlmcsd
     update_nss_diag
-    update_menu_location
+    # update_menu_location
     fix_compile_coremark
     update_dnsmasq_conf
     add_backup_info_to_sysupgrade
